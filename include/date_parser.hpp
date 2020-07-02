318e94af78610f0f3b54c0b0e4d20f3821df57cc
yumetodo <yume-wikijp@live.jp>
2020-07-02 21:25:39 +0900

    feat(data_parser): add test

---
+++ include/date_parser.hpp
@@ -0,0 +1,8 @@
+#ifndef APACHE2_LOG_ANALYZER_DATE_PARSER_HPP_
+#define APACHE2_LOG_ANALYZER_DATE_PARSER_HPP_
+#include <date/date.h>
+inline date::sys_seconds date_parser(std::string_view s)
+{
+    return {};
+}
+#endif
---
+++ test/date_parser.cpp
@@ -0,0 +1,23 @@
+#include <date/date.h>
+#include <utility>
+#include "../include/date_parser.hpp"
+#include "../iutest/include/iutest.hpp"
+IUTEST(DateParser, DateParser)
+{
+    using namespace date::literals;
+    using namespace std::chrono_literals;
+    constexpr std::pair<const char*, date::sys_seconds> input[] = {
+        {
+            "30/Jun/2020:11:30:43 +0900",
+            date::sys_days(2020_y/6/30) + 11h + 30min + 43s - 9h
+        },
+        {
+            "10/Oct/2000:13:55:36 -0700",
+            date::sys_days(2000_y/10/10) + 13h + 55min + 36s +7h
+        }
+    };
+    for (auto&& [i, expected] : input) {
+        const auto re = date_parser(i);
+        IUTEST_ASSERT_EQ(expected, re);
+    }
+}
