95cb61e23692c120c25f3d7f7c869f83cbd9b864
yumetodo <yume-wikijp@live.jp>
2020-07-03 11:56:49 +0900

    feat(count_by_remote): add impl

--- include/count_by_remote.hpp
+++ include/count_by_remote.hpp
@@ -16,9 +16,42 @@ public:
     count_by_remote& operator=(count_by_remote&&) = default;
     void read(const raw_parsed_line& line, const options& o)
     {
+        if (need_skip_read(line.time, o)) return;
+        ++this->table_[std::string(line.host)];
     }
+private:
+    template<typename Compare>
+    void print_impl(std::ostream& os, std::size_t take, Compare&& comp)
+    {
+        std::vector<tmp_array_elem_t> tmp(this->table_.begin(), this->table_.end());
+        std::sort(tmp.begin(), tmp.end(), std::forward<Compare>(comp));
+        std::size_t n = 0;
+        for (auto&& [h, c] : tmp) {
+            if (c != 0) {
+                os << h << ':' << c << '\n';
+                if (++n == take) break;
+            }
+        }
+    }
+public:
     void print(std::ostream& os, const options& o)
     {
+        switch(o.order) {
+        case order_mode::ascending:
+            this->print_impl(os, o.take, [](const tmp_array_elem_t& l, const tmp_array_elem_t& r){
+                return l.second < r.second;
+            });
+            break;
+        case order_mode::descending:
+            this->print_impl(os, o.take, [](const tmp_array_elem_t& l, const tmp_array_elem_t& r){
+                return l.second > r.second;
+            });
+            break;
+        default:
+            throw std::invalid_argument("not known such a order_mode");
+            break;
+        }
+        os << std::flush;
     }
 };
 #endif
