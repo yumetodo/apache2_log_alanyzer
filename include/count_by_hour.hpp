7b14d84b1448e90f051757ce5cc47f5c412db3b8
yumetodo <yume-wikijp@live.jp>
2020-07-03 11:17:27 +0900

    feat(count_by_hour): add impl

--- include/count_by_hour.hpp
+++ include/count_by_hour.hpp
@@ -1,4 +1,4 @@
-#ifndef APACHE2_LOG_ANALYZER_COUNT_BY_HOUR_HPP_
+﻿#ifndef APACHE2_LOG_ANALYZER_COUNT_BY_HOUR_HPP_
 #define APACHE2_LOG_ANALYZER_COUNT_BY_HOUR_HPP_
 #include "cmd_argument_parser.hpp"
 #include "line_splitter.hpp"
@@ -23,12 +23,65 @@ public:
     count_by_hour& operator=(count_by_hour&&) = default;
     void read(date::sys_seconds tp)
     {
+        const auto dp = std::chrono::floor<date::days>(tp);
+        const auto h = date::hh_mm_ss{tp - dp}.hours().count();
+        ++this->table_.at(h - 1);
     }
     void read(const raw_parsed_line& line, const options& o)
     {
+        const auto date = date_parser(line.time);
+        if (date < o.since) return;
+        if (o.until != date::sys_seconds{} && o.until < date) return;
+        this->read(date);
     }
+private:
+    template<typename Compare>
+    void print_impl(std::ostream& os, std::size_t take, Compare&& comp)
+    {
+        tmp_array_t tmp;
+        std::size_t j = 0;
+        std::transform(
+            this->table_.begin(), this->table_.end(), tmp.begin(),
+            [&j](std::size_t e) { return tmp_array_elem_t{j++, e}; }
+        );
+        std::sort(tmp.begin(), tmp.end(), std::forward<Compare>(comp));
+        std::size_t n = 0;
+        for (auto&& [i, e] : tmp) {
+            if (e != 0) {
+                os << std::setw(2) << std::setfill('0') << i + 1 << ':' << e << '\n';
+                if (++n == take) break;
+            }
+        }
+    }
+public:
     void print(std::ostream& os, const options& o)
     {
+        switch(o.order) {
+        case order_mode::chronological:
+            {
+                std::size_t i = 1;
+                for (auto&& e : this->table_) {
+                    if (e != 0) {
+                        os << std::setw(2) << std::setfill('0') << i << ':' << e << '\n';
+                    }
+                    ++i;
+                }
+            }
+            break;
+        case order_mode::ascending:
+            this->print_impl(os, o.take, [](const tmp_array_elem_t& l, const tmp_array_elem_t& r){
+                return l.second < r.second;
+            });
+            break;
+        case order_mode::descending:
+            this->print_impl(os, o.take, [](const tmp_array_elem_t& l, const tmp_array_elem_t& r){
+                return l.second > r.second;
+            });
+            break;
+        default:
+            break;
+        }
+        os << std::flush;
     }
 };
 #endif
